a79779fde3aec37840d7156cb63dee7bcd5120f6
James Gadoury <gadouryjames@gmail.com>
2021-09-05 18:21:51 -0400

    Make condition to check for frames configurable with new classes ConditionalFrameEvaluator and MultipleFaceChecker. Add debug cli flag

--- main.py
+++ main.py
@@ -3,15 +3,44 @@ import argparse
 from video import VideoCaptureWindow
 from faceclassifier import FaceClassifier
 
+class ConditionalFrameEvaluator:
+    def __init__(self, condition_checker, threshold):
+        self.condition_checker = condition_checker
+        self.threshold = threshold
+        self.counter = 0
+
+    def evaluate(self, frame):
+        if self.condition_checker.check(frame):
+            self.counter += 1
+        if self.counter == self.threshold:
+            self.counter = 0
+            return True
+        return False
+
+
+class MultipleFaceChecker:
+    def __init__(self):
+        self.face_classifier = FaceClassifier()
+
+    def check(self, frame):
+        # Detect faces
+        faces = self.face_classifier.detect_faces(frame)
+        return len(faces) > 1
+
+def mutate_frame_with_face_rectangles(frame):
+    face_classifier = FaceClassifier()
+    faces = face_classifier.detect_faces(frame)
+    # Draw rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
 def main(args):
     # define a video capture object
     video_capture = cv2.VideoCapture(0)
 
     window = VideoCaptureWindow()
 
-    face_classifier = FaceClassifier()
-
-    multiple_faces_detected_count = 0
+    conditional_evaluator = ConditionalFrameEvaluator(MultipleFaceChecker(), args.frames)
 
     while(True):
         # Capture the video frame by frame
@@ -19,19 +48,11 @@ def main(args):
 
         window.generate()
 
-        # Detect faces
-        faces = face_classifier.detect_faces(frame)
-
-        if len(faces) > 1:
-            multiple_faces_detected_count += 1
-
-        if multiple_faces_detected_count == args.frames:
+        if conditional_evaluator.evaluate(frame):
             print("Multiple faces detected for a while!")
-            multiple_faces_detected_count = 0
 
-        # Draw rectangle around the faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        if args.debug:
+            mutate_frame_with_face_rectangles(frame)
 
         # Display the resulting frame
         window.update_frame(frame)
@@ -48,4 +69,5 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--frames', type=int, help='number of frames that satisfies a condition before sending slack message')
+    parser.add_argument('--debug', action='store_true', help='flag turns on debug logic')
     main(parser.parse_args())
