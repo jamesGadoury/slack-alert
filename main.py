43d9b5ef7ccab87787132735dc4122201a0f5a9e
James Gadoury <gadouryjames@gmail.com>
2021-09-05 17:43:15 -0400

    Start refactoring, create FaceClassifier class and generate_window function

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import cv2
 import argparse
 
+
+WINDOW_NAME = 'VideoCapture'
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+def generate_window():
+    cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(WINDOW_NAME, WINDOW_WIDTH, WINDOW_HEIGHT)
+
+class FaceClassifier:
+    def __init__(self):
+        # Load the cascade
+        self.faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    def detect_faces(self, frame):
+        assert(len(frame.shape)==3)
+        # assuming frame is in BGR
+        # Convert into grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        return self.faceCascade.detectMultiScale(gray, 1.1, 4)
+
 def main(args):
     # define a video capture object
     videoCapture = cv2.VideoCapture(0)
 
-    # Load the cascade
-    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-    windowName = 'VideoCapture'
-    windowWidth = 1280
-    windowHeight = 720
+    faceClassifier = FaceClassifier()
 
     multipleFacesDetectedCount = 0
 
@@ -18,14 +34,10 @@ def main(args):
         # Capture the video frame by frame
         ret, frame = videoCapture.read()
 
-        cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
-        cv2.resizeWindow(windowName, windowWidth, windowHeight)
-
-        # Convert into grayscale
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        generate_window()
 
         # Detect faces
-        faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+        faces = faceClassifier.detect_faces(frame)
 
         if len(faces) > 1:
             multipleFacesDetectedCount += 1
@@ -39,7 +51,7 @@ def main(args):
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
         # Display the resulting frame
-        cv2.imshow(windowName, frame)
+        cv2.imshow(WINDOW_NAME, frame)
 
         # the 'q' button is set as the quitting button
         if cv2.waitKey(1) & 0xFF == ord('q'):
