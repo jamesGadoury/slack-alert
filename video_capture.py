13a511e3a5c9a98b09a09873d3acc4b00ee298ec
James Gadoury <gadouryjames@gmail.com>
2021-09-05 17:24:02 -0400

    Initial commit of face detect logic

---
+++ video_capture.py
@@ -0,0 +1,54 @@
+# import the opencv library
+import cv2
+
+def main():
+    # define a video capture object
+    videoCapture = cv2.VideoCapture(0)
+
+    # Load the cascade
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    windowName = 'VideoCapture'
+    windowWidth = 1280
+    windowHeight = 720
+
+    multipleFacesDetectedCount = 0
+
+    while(True):
+        # Capture the video frame by frame
+        ret, frame = videoCapture.read()
+
+        cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(windowName, windowWidth, windowHeight)
+
+        # Convert into grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces
+        faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+
+        if len(faces) > 1:
+            multipleFacesDetectedCount += 1
+
+        if multipleFacesDetectedCount > 30:
+            print("Multiple faces detected for a while!")
+
+        # Draw rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+        # Display the resulting frame
+        cv2.imshow(windowName, frame)
+
+        # the 'q' button is set as the quitting button
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the video capture object
+    videoCapture.release()
+
+    # Destroy all the windows
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
